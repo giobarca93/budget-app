4fcf1eb83c42b6bc59eef5e86897f16a29fb4702
Giovani Minchola <wgmin1@student.monash.edu>
2019-11-06 17:34:19 +1100

    controller addItem is passing data

--- app.js
+++ app.js
@@ -1,31 +1,34 @@
 
-
+// Budget Controller 
 var budgetController = (function() {
-    var x = 23;
-
-    var add = function(a){
-        return x + a;
-    }
-
-    return {
-        publicTest: function(b){
-            return add(b);
-        }
-    }
-
+    
 })();
 
-
+// UI controller 
 var UIController = (function() {
 
 })();
 
+// Global app controller
 var controller = (function(budgetCtrl, uiCtrl) {
-    var z = budgetCtrl.publicTest(5);
-    return {
-        anotherPublic: function() {
-            console.log(z);
-        }
+
+    var ctrlAddItem = function(){
+        //1. get the field input data
+        //2. add the item to the budget controller
+        //3. add the item to the UI
+        //4. calculate the budget
+        //5. display the budget on the UI
+        console.log("it works");
+
     }
+
+    document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+
+    document.addEventListener("keypress", function(event){
+        if(event.keyCode === 13 || event.which === 13){
+            ctrlAddItem();
+        }
+    });
+    
 
 })(budgetController, UIController);
