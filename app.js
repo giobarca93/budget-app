5decca075ad5f9b0330f91a7752820c6f50370a2
Giovani Minchola <wgmin1@student.monash.edu>
2019-11-21 22:23:44 +1100

    UI display now display budget and shows all calculations and percentages

--- app.js
+++ app.js
@@ -101,7 +101,11 @@ var UIController = (function() {
         inputValue: ".add__value",
         inputBtn: ".add__btn",
         incomeContainer: ".income__list",
-        expensesContainer: ".expenses__list"
+        expensesContainer: ".expenses__list",
+        budgetLabel: ".budget__value",
+        incomeLabel: ".budget__income--value",
+        expensesLabel: ".budget__expenses--value",
+        percentageLabel: ".budget__expenses--percentage"
 
     };
 
@@ -148,6 +152,18 @@ var UIController = (function() {
             fieldsArr[0].focus();
         },
 
+        displayBudget: function(obj){
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+            
+            if(obj.percentage > 0){
+                document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + "%";
+            }else{
+                document.querySelector(DOMstrings.percentageLabel).textContent = "---";
+            }
+        },
+
         getDOMstrings: function(){
             return DOMstrings;
         }
@@ -178,8 +194,8 @@ var controller = (function(budgetCtrl, UICtrl) {
         var budget = budgetCtrl.getBudget();
 
         //3. display the budget on the UI
-        console.log(budget);  
-    };
+        UICtrl.displayBudget(budget);   
+    }; 
 
  
     var ctrlAddItem = function(){
@@ -204,7 +220,13 @@ var controller = (function(budgetCtrl, UICtrl) {
     return {
         init: function(){
             console.log("app has started");
+            UICtrl.displayBudget({
+                budget: 0,
+                totalInc: 0,
+                totalExp: 0,
+                percentage: -1});
             setupEventListeners();
+             
         }
     };
 
